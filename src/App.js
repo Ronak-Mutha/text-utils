9d3282ae16b40a5c237079a1d94ddf7416b76c7f
Ronak Mutha <mutharonak97@gmail.com>
2021-12-04 13:44:54 +0530

    feat: add routing for home and about

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Alert from "./components/Alert";
 import TextForm from "./components/TextForm";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <About />
-      <br />
-      <Alert />
-      <br />
-      <TextForm />
-      <br />
+      <Router>
+        <Navbar />
+        <Alert alert={alert} />
+        <Routes>
+          <Route exact path="/" element={<TextForm />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
