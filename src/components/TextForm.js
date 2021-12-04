aaaa5337ab328970960e7b7a4dff18dbbe6672b8
Ronak Mutha <mutharonak97@gmail.com>
2021-12-04 14:47:31 +0530

    feat: convert to uppercase

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,9 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 
 export default function Textform({ showAlert }) {
-  useEffect(() => {
-    showAlert("Alert added!", "success");
-  }, []);
+  const [text, setText] = useState("");
+
+  const handleOnChange = (e) => setText(e.target.value);
+  const convertToUpperCase = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    showAlert("Converted to uppercase!", "success");
+  };
   return (
     <>
       <div className="container">
@@ -11,17 +16,43 @@ export default function Textform({ showAlert }) {
           TextUtils - word counter, character counter, remove extra spaces
         </h1>
         <div className="mb-3">
-          <textarea className="form-control" id="myBox" rows="8"></textarea>
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
         </div>
-        <button className="btn btn-primary mx-1 my-1">
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary mx-1 my-1"
+          onClick={convertToUpperCase}
+        >
           Convert to Uppercase
         </button>
-        <button className="btn btn-primary mx-1 my-1">
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary mx-1 my-1"
+        >
           Convert to Lowercase
         </button>
-        <button className="btn btn-primary mx-1 my-1">Clear Text</button>
-        <button className="btn btn-primary mx-1 my-1">Copy Text</button>
-        <button className="btn btn-primary mx-1 my-1">
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary mx-1 my-1"
+        >
+          Clear Text
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary mx-1 my-1"
+        >
+          Copy Text
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary mx-1 my-1"
+        >
           Remove Extra Spaces
         </button>
       </div>
@@ -29,8 +60,8 @@ export default function Textform({ showAlert }) {
         <h2>Current text summary:</h2>
         <p>Text summary will be shown here</p>
 
-        <h2>Manipulated Text</h2>
-        <p>Manipulated text will be shown here</p>
+        <h2>Preview</h2>
+        <p>{text.length > 0 ? text : "Nothing to preview!"}</p>
       </div>
     </>
   );
